ae0cd2ddf4e54aa75b0fdcf0453e2eaad9fe976a
Eric Conlon <ejconlon@gmail.com>
2016-11-16 11:57:15 -0800

    client start

--- src/Notate/Client.hs
+++ src/Notate/Client.hs
@@ -1,16 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Notate.Client where
+module Notate.Client
+  ( DisplayData(..)
+  , emitText
+  , emitDisplayData
+  , hasDisplayData
+  , readAllDisplayData
+  , clearAllDisplayData
+  ) where
 
 import Control.Monad.IO.Class
 import qualified Data.Aeson as A
 import Data.Aeson ((.=), (.:))
 import Data.Aeson.Types (typeMismatch)
+import Data.Monoid (mempty)
+import Data.HashMap.Strict as HMS
+import Data.Text (Text)
 import System.Environment (lookupEnv)
 
 data DisplayData = DisplayData
-  { ddData :: A.Object
-  , ddMetadata :: A.Object
+  { ddData :: A.Value
+  , ddMetadata :: A.Value
   } deriving (Show, Eq)
 
 instance A.ToJSON DisplayData where
@@ -26,9 +36,28 @@ instance A.FromJSON DisplayData where
 magicFileKey :: String
 magicFileKey = "NOTATE_DISPLAY_FILE"
 
-emitDisplayData :: MonadIO m => DisplayData -> m ()
-emitDisplayData dd = do
+emitText :: MonadIO m => Text -> Text -> m ()
+emitText ty con  = emitDisplayData dd
+  where
+    dat = A.Object (HMS.singleton ty (A.String con))
+    meta = A.Object (HMS.empty)
+    dd = DisplayData dat meta
+
+withMagicFile :: MonadIO m => (FilePath -> m ()) -> m ()
+withMagicFile act = do
   mf <- liftIO $ lookupEnv magicFileKey
   case mf of
     Nothing -> return ()
-    Just file -> undefined
+    Just file -> act file
+
+emitDisplayData :: MonadIO m => DisplayData -> m ()
+emitDisplayData dd = undefined
+
+hasDisplayData :: MonadIO m => m Bool
+hasDisplayData = undefined
+
+readAllDisplayData :: MonadIO m => m [DisplayData]
+readAllDisplayData = undefined
+
+clearAllDisplayData :: MonadIO m => m ()
+clearAllDisplayData = undefined
